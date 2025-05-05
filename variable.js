55b9d5b2c4091bd3ceb79aa45569f3feb76937e3
puja kumari <pkumari41038@gmail.com>
2025-05-05 12:20:27 +0530

    practice variables

---
+++ variable.js
@@ -0,0 +1,18 @@
+// Practice variables
+
+const accountId = 12345;
+let accountEmail = "puja@gmail.com";
+var accountPassword = "12345678"
+accountCity = "Boakro"
+let accountState;
+
+
+// 
+// accountId = "4576"
+// console.log(accountId);
+
+accountEmail= "kri@gmail.com";
+accountPassword= "2345";
+accountCity= "Bengaluru";
+
+console.table = ([accountId, accountEmail, accountPassword, accountCity, accountState])
