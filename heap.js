001ba7a2e70230d07b03331d2c5380ab34ad1862
puja kumari <pkumari41038@gmail.com>
2025-05-06 18:28:01 +0530

    memory

---
+++ heap.js
@@ -0,0 +1,21 @@
+//  heap - prmitive , heap (non-primitive)
+
+let myInstapage = "hipuja.com"
+let anothername = myInstapage
+
+anothername = "newpuja"
+
+console.log(anothername);
+console.log(myInstapage);
+
+let userOne ={
+    email: "user@gogle.com",
+    upi: "user@ybl"
+
+}
+let userTwo = userOne
+
+userTwo.email = "puja@gmail.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
