cb80295df7a6b724df591094c53a16a9b6cd826d
puja kumari <pkumari41038@gmail.com>
2025-05-08 15:15:47 +0530

    comparison

---
+++ coversion.js
@@ -0,0 +1,20 @@
+let value = 2
+let negValue = -value
+// console.log(negValue); 
+
+let str1 = "puja"
+let str ="kumari"
+
+let str2= str1 + str
+// console.log(str2);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(+true)
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
---
+++ datatypes.js
@@ -0,0 +1,58 @@
+//  treat all js code as newr version
+"use strict";    
+
+let name = "puja"
+let age = 23
+let isLoggedIn = false
+
+
+// number 
+// bigint
+// string ""
+//  boolean = true/false
+// null  standalone value
+// undefined 
+// symbol -unique
+
+// object
+
+// console.log (typeof "puja");
+// console.log (typeof null);
+// console.log (typeof undefined);
+
+
+
+
+
+// primitive 7types  - function object al
+
+// primitive call by value" 
+// means that when you pass a primitive data 
+// type (like number, string, boolean,
+//  null, undefined, symbol, or bigint) to a function, 
+// a copy of the value is passed. So, 
+// changes made to that parameter inside the function do not affect the original value outside the function.
+
+// string , number , boolean , null, symbol, undefined,
+
+// refrance (non primitive) when you pass a non-primitive value to a function, you are passing a reference to the original value, not a copy.
+
+// So, if you change the value inside the function, 
+// it also changes outside the function.
+
+
+// array, objects, functin
+const flowers = ["rose", "sunflower", "lily"]
+{
+    name : "puja",
+    age: 23,
+}
+
+const myFunction = function(){
+    console.log ("hello");
+
+}
+
+console.log (typeof bigNumber);
+
+
